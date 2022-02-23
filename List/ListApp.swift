75488ff857ab3e90d76e1857f3a1e6e6e8c92127
Kenzo <khoiduc1997@gmail.com>
2022-02-23 02:05:06 -0500

    first commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Kenzo on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
