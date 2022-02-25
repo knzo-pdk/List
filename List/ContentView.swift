40dcee033f58c1fc605cab202c5d99e8b675d353
Kenzo <khoiduc1997@gmail.com>
2022-02-25 16:31:33 -0500

    third push

--- List/ContentView.swift
+++ List/ContentView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List{
-            Text("Walk the dog")
-            Text("Brush my teeth")
-            Text("Learn iOS development")
-            Text("Soccer practice")
-            Text("Eat ice cream")
-
+        NavigationView{
+            List{
+                Text("Walk the dog")
+                Text("Brush my teeth")
+                Text("Learn iOS development")
+                Text("Soccer practice")
+                Text("Eat ice cream")
+            }
+            .navigationTitle("Checklist")
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
