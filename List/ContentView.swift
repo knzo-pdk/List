b261eabce9d0c27173279ab4614b1a5e632d34c0
Kenzo <khoiduc1997@gmail.com>
2022-03-01 17:51:20 -0500

    Fourth push

--- List/ContentView.swift
+++ List/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Checklist")
-            listStyle(GroupedListStyle())
+            .listStyle(GroupedListStyle())
         }
     }
 }
